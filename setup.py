da23b79352a156ac7e5e3575a349d0f22c8ba316
Raul Gil <raulgil@Rauls-MacBook-Pro.local>
2015-08-25 00:54:22 -0400

    removing .idea directory. added url homepage for project

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email="gilraul90@gmail.com",
     packages=["src", "helpers"],
     include_package_data=True,
+    url='http://github.com/rgil90/nbapipy',
     # license="LICENSE.txt",
     description="API wrapper around the erikberg mlb/nba web service",
     install_requires=["requests", ]
