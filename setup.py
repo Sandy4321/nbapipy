78c5e163fcadc281e4c2ed4d511759ce02e10127
Raul Gil <raulgil@Rauls-MacBook-Pro-2.local>
2015-09-25 00:44:01 -0400

    modified setup.py file

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name="nbapipy",
-    version="0.1.0",
+    version="0.1.1",
     author="Raul Gil",
     author_email="gilraul90@gmail.com",
-    packages=["src", "helpers"],
     include_package_data=True,
     url='http://github.com/rgil90/nbapipy',
     # license="LICENSE.txt",
     description="API wrapper around the erikberg mlb/nba web service",
-    install_requires=["requests", ]
-
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+    ]
 )
